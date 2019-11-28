2ba05c0a975b005e76f4ac26217d0527bbb9941b
Ilyas Ezzahid <ezzil1997@gmail.com>
2019-11-29 01:47:34 +0700

    adding filters

--- app.js
+++ app.js
@@ -9,33 +9,85 @@ let img = new Image();
 let fileName = "";
 
 //Upload File
-uploadFile.addEventListener("change", e => {
-  // get file
-  const file = document.getElementById("upload-file").files[0];
+uploadFile.addEventListener(
+  "change",
+  e => {
+    // get file
+    const file = document.getElementById("upload-file").files[0];
 
-  // init FileReader
-  const reader = new FileReader();
+    // init FileReader
+    const reader = new FileReader();
 
-  if (file) {
-    // set File Name
-    fileName = file.name;
-    // Read data as url
-    reader.readAsDataURL(file);
+    if (file) {
+      // set File Name
+      fileName = file.name;
+      // Read data as url
+      reader.readAsDataURL(file);
 
-    // Add image to Canvas
-    reader.addEventListener("load", () => {
-      // creating the image
-      img = new Image();
-      img.src = reader.result;
+      // Add image to Canvas
+      reader.addEventListener("load", () => {
+        // creating the image
+        img = new Image();
+        img.src = reader.result;
 
-      // on Image load add to Canvas
-      img.onload = function() {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        // drawImage(the image itself, when to start from x, ehen to start from y)
-        ctx.drawImage(img, 0, 0);
-        canvas.removeAttribute("data-caman-id");
-      };
-    });
+        // on Image load add to Canvas
+        img.onload = function() {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          // drawImage(the image itself, when to start from x, ehen to start from y)
+          ctx.drawImage(img, 0, 0);
+          canvas.removeAttribute("data-caman-id");
+        };
+      });
+    }
+  },
+  false
+);
+
+// Filters
+document.addEventListener("click", e => {
+  if (e.target.classList.contains("filter-btn")) {
+    if (e.target.classList.contains("brightness-add")) {
+      Caman("#canvas", img, function() {
+        this.brightness(5).render();
+      });
+    } else if (e.target.classList.contains("brightness-remove")) {
+      Caman("#canvas", img, function() {
+        this.brightness(-5).render();
+      });
+    } else if (e.target.classList.contains("contrast-add")) {
+      console.log(123);
+      Caman("#canvas", img, function() {
+        this.contrast(5).render();
+      });
+    } else if (e.target.classList.contains("contrast-remove")) {
+      console.log(123);
+
+      Caman("#canvas", img, function() {
+        this.contrast(-5).render();
+      });
+    } else if (e.target.classList.contains("saturation-add")) {
+      console.log(123);
+
+      Caman("#canvas", img, function() {
+        this.saturation(5).render();
+      });
+    } else if (e.target.classList.contains("saturation-remove")) {
+      Caman("#canvas", img, function() {
+        this.saturation(-5).render();
+      });
+    } else if (e.target.classList.contains("vibrance-add")) {
+      Caman("#canvas", img, function() {
+        this.vibrance(5).render();
+      });
+    } else if (e.target.classList.contains("vibrance-remove")) {
+      Caman("#canvas", img, function() {
+        this.vibrance(-5).render();
+      });
+    } else if (e.target.classList.contains("vibrance-remove")) {
+      Caman("#canvas", img, function() {
+        this.vibrance(-5).render();
+      });
+    }
   }
 });
